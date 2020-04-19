345d29181649996f08b2c30878ee0b7827d0a934
stefaleon <stefaleon@gmail.com>
2020-04-19 16:24:40 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const App = () => {
-  return 'cra clean up';
+  return (
+    <Navbar>
+      <li>child 1</li>
+      <li>child 2</li>
+      <li>child 3</li>
+    </Navbar>
+  );
+};
+
+const Navbar = (props) => {
+  return (
+    <nav className='navbar'>
+      <ul className='navbar-nav'>{props.children}</ul>
+    </nav>
+  );
 };
 
 export default App;
