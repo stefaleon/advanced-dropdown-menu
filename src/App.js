9ab7f2788c0a63b68466d95084b095aef5ee9835
stefaleon <stefaleon@gmail.com>
2020-04-19 21:27:37 +0300

    CSSTransition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { ReactComponent as BellIcon } from './icons/bell.svg';
 import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
 import { ReactComponent as CaretIcon } from './icons/caret.svg';
@@ -7,6 +7,7 @@ import { ReactComponent as CogIcon } from './icons/cog.svg';
 import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
 import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
 import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+import { CSSTransition } from 'react-transition-group';
 
 const App = () => {
   return (
@@ -42,24 +43,97 @@ const NavItem = (props) => {
   );
 };
 
-const DropdownItem = (props) => {
-  return (
-    <a href='#' className='menu-item'>
-      <span className='icon-button'>{props.leftIcon}</span>
-      {props.children}
-    </a>
-  );
-};
-
 const DropdownMenu = () => {
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight);
+  }, []);
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+  const DropdownItem = (props) => {
+    return (
+      <a
+        href='#'
+        className='menu-item'
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
+        <span className='icon-button'>{props.leftIcon}</span>
+        {props.children}
+        <span className='icon-right'>{props.rightIcon}</span>
+      </a>
+    );
+  };
+
   return (
-    <div className='dropdown'>
-      <DropdownItem leftIcon={<CogIcon />}>
-        <h2>Menu Item 1</h2>
-      </DropdownItem>
-      <DropdownItem leftIcon={<ChevronIcon />}>
-        <h2>Menu Item 2</h2>
-      </DropdownItem>
+    <div className='dropdown' style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
+        in={activeMenu === 'main'}
+        timeout={500}
+        classNames='menu-primary'
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className='menu'>
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu='settings'
+          >
+            Settings
+          </DropdownItem>
+          <DropdownItem
+            leftIcon='🦧'
+            rightIcon={<ChevronIcon />}
+            goToMenu='animals'
+          >
+            Animals
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'settings'}
+        timeout={500}
+        classNames='menu-secondary'
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className='menu'>
+          <DropdownItem goToMenu='main' leftIcon={<ArrowIcon />}>
+            <h2>My Tutorial</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>HTML</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>CSS</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>JavaScript</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>Awesome!</DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'animals'}
+        timeout={500}
+        classNames='menu-secondary'
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className='menu'>
+          <DropdownItem goToMenu='main' leftIcon={<ArrowIcon />}>
+            <h2>Animals</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon='🦘'>Kangaroo</DropdownItem>
+          <DropdownItem leftIcon='🐸'>Frog</DropdownItem>
+          <DropdownItem leftIcon='🦋'>Horse?</DropdownItem>
+          <DropdownItem leftIcon='🦔'>Hedgehog</DropdownItem>
+        </div>
+      </CSSTransition>
     </div>
   );
 };
